bcba93480a3e06fc990858c53a0b2c7ace3233cc
Husensei <husensei.18@gmail.com>
2023-07-31 11:47:04 +0700

    Add some changes

--- index.js
+++ index.js
@@ -16,9 +16,3 @@ client.connect((err) => {
     console.log("Connected");
   }
 });
-
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_PORT);
-console.log(process.env.DATABASE);
