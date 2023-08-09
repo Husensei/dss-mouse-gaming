d5ab370f18fff1c0470a78e483557561d56965f2
Husensei <husensei.18@gmail.com>
2023-08-09 16:46:46 +0700

    change variable name

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DATABASE,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
 });
 
 module.exports = client;
