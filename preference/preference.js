479baa723ceeb4e3608f03b3581e314ddfbe633f
Husensei <husensei.18@gmail.com>
2023-08-20 04:07:27 +0700

    Fix bug

--- preference/preference.js
+++ preference/preference.js
@@ -13,15 +13,15 @@ const getMatrix = async () => {
 const insertMatrix = async (data) => {
   return new Promise(async (resolve, reject) => {
     const queries = [];
-    for (var i = 0; i < data.value.length; i++) {
-      queries.push(`INSERT INTO pairwise_comparison (id_criteria1, id_criteria2, value) VALUES ('${data.value[i].row}', '${data.value[i].column}', '${data.value[i].value}'`);
+    for (var i = 0; i < data.scale.length; i++) {
+      queries.push(`INSERT INTO pairwise_comparison (id_criteria1, id_criteria2, value) VALUES ('${data.scale[i].row}', '${data.scale[i].column}', ${data.scale[i].value})`);
     }
     try {
       const reset = await client.query(`DELETE FROM pairwise_comparison WHERE EXISTS (SELECT 1 FROM pairwise_comparison);`);
       for (const query of queries) {
         const result = await client.query(query);
       }
-      resolve({ staus: 201, message: "Insert success" });
+      resolve({ status: 201, message: "Insert success" });
     } catch (error) {
       reject(error);
     }
@@ -40,6 +40,8 @@ const getPreference = async () => {
 
 const insertPreference = async (data) => {
   return new Promise(async (resolve, reject) => {
+    const reset = await client.query(`DELETE FROM preference WHERE EXISTS (SELECT 1 FROM preference);`);
+
     client.query(
       `INSERT INTO preference (ergo_pref, ambi_pref, wireless_pref, wired_pref, palm_pref, claw_pref, fingertip_pref, price_pref) VALUES ('${data.ergo_pref}', '${data.ambi_pref}', '${data.wireless_pref}', '${data.wired_pref}', '${data.palm_pref}', '${data.claw_pref}', '${data.fingertip_pref}', '${data.price_pref}')`
     );
